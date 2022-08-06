dca8664542ed4d997fe1c69753c8a4df738dd349
JCRamires <jcramires851@gmail.com>
2022-08-06 13:11:02 -0300

    Exporting vuex props

--- src/stores/jumboStores.ts
+++ src/stores/jumboStores.ts
@@ -1,5 +1,12 @@
 import { InjectionKey } from "vue";
-import { createStore, Store, useStore as baseUseStore } from "vuex";
+import {
+  createStore,
+  Store,
+  useStore as baseUseStore,
+  GetterTree,
+  MutationTree,
+  ActionTree,
+} from "vuex";
 
 export interface JumboStore {
   addressName: string;
@@ -33,70 +40,81 @@ export interface State {
 
 export const key: InjectionKey<Store<State>> = Symbol();
 
+export const state = {
+  isLoading: true,
+  stores: [],
+};
+
+export const getters: GetterTree<State, any> = {
+  getCities(state) {
+    const citiesSet = state.stores.reduce((acc, store) => {
+      if (store.city) {
+        acc.add(store.city);
+      }
+
+      return acc;
+    }, new Set<string>());
+
+    return citiesSet;
+  },
+  getCitiesByName: (_, getters) => (cityName: string) => {
+    const cities: string[] = Array.from(getters.getCities);
+    return cities.filter((city) =>
+      city.toLowerCase().includes(cityName.toLowerCase())
+    );
+  },
+  getStores(state): JumboStore[] {
+    const jumboStores = state.stores.map((jumboStore) => ({
+      ...jumboStore,
+      addressName: jumboStore.addressName.slice(6),
+    }));
+    return jumboStores;
+  },
+  getStoreById: (_, getters) => (storeId: string) =>
+    getters.getStores.filter(
+      (jumboStore: JumboStore) => jumboStore.sapStoreID === storeId
+    )[0],
+  getStoresByCity: (_, getters) => (cityName: string) =>
+    getters.getStores.filter(
+      (jumboStore: JumboStore) =>
+        jumboStore.city.toLowerCase() === cityName.toLowerCase()
+    ),
+  getStoresBySearchTerm: (_, getters) => (searchTerm: string) =>
+    getters.getStores.filter(
+      (jumboStore: JumboStore) =>
+        jumboStore.addressName
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase()) ||
+        jumboStore.city.toLowerCase().includes(searchTerm.toLowerCase())
+    ),
+};
+
+export const mutations: MutationTree<State> = {
+  setIsLoading(state, isLoading) {
+    state.isLoading = isLoading;
+  },
+  setStores(state, stores) {
+    state.stores = stores;
+  },
+};
+
+export const actions: ActionTree<State, any> = {
+  async fetchStores(context) {
+    context.commit("setIsLoading", true);
+    const response = await fetch(
+      "https://api.jsonstorage.net/v1/json/00000000-0000-0000-0000-000000000000/c4357a15-46e2-4542-8e93-6aa6a0c33c1e"
+    );
+    const storesJson = await response.json();
+    context.commit("setStores", storesJson);
+    context.commit("setIsLoading", false);
+  },
+};
+
 export const jumboStores = createStore<State>({
-  state() {
-    return {
-      isLoading: true,
-      stores: [],
-    };
-  },
-  getters: {
-    getCities(state) {
-      const citiesSet = state.stores.reduce((acc, store) => {
-        if (store.city) {
-          acc.add(store.city);
-        }
-
-        return acc;
-      }, new Set<string>());
-
-      return citiesSet;
-    },
-    getCitiesByName: (_, getters) => (cityName: string) => {
-      const cities: string[] = Array.from(getters.getCities);
-      return cities.filter((city) => city.toLowerCase().includes(cityName.toLowerCase()));
-    },
-    getStores(state): JumboStore[] {
-      const jumboStores = state.stores.map((jumboStore) => ({
-        ...jumboStore,
-        addressName: jumboStore.addressName.slice(6),
-      }));
-      return jumboStores;
-    },
-    getStoreById: (_, getters) => (storeId: string) =>
-      getters.getStores.filter(
-        (jumboStore: JumboStore) => jumboStore.sapStoreID === storeId
-      )[0],
-    getStoresByCity: (_, getters) => (cityName: string) =>
-      getters.getStores.filter(
-        (jumboStore: JumboStore) => jumboStore.city.toLowerCase() === cityName.toLowerCase()
-      ),
-    getStoresBySearchTerm: (_, getters) => (searchTerm: string) =>
-      getters.getStores.filter(
-        (jumboStore: JumboStore) =>
-          jumboStore.addressName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          jumboStore.city.toLowerCase().includes(searchTerm.toLowerCase())
-      ),
-  },
-  mutations: {
-    setIsLoading(state, isLoading) {
-      state.isLoading = isLoading;
-    },
-    setStores(state, stores) {
-      state.stores = stores;
-    },
-  },
-  actions: {
-    async fetchStores(context) {
-      context.commit("setIsLoading", true);
-      const response = await fetch(
-        "https://api.jsonstorage.net/v1/json/00000000-0000-0000-0000-000000000000/c4357a15-46e2-4542-8e93-6aa6a0c33c1e"
-      );
-      const storesJson = await response.json();
-      context.commit("setStores", storesJson);
-      context.commit("setIsLoading", false);
-    },
-  },
+  state,
+  getters,
+  mutations,
+  actions,
 });
 
 export function useStore() {
