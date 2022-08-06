86992bfb6b53b57dc175b335f53106ae54cba4f7
JCRamires <jcramires851@gmail.com>
2022-08-06 12:44:46 -0300

    removing key

--- src/components/StoresList.vue
+++ src/components/StoresList.vue
@@ -11,6 +11,6 @@ const { stores } = defineProps<Props>();
 
 <template>
   <div class="grid md:grid-cols-[repeat(auto-fit,_minmax(25vw,_1fr))] gap-2">
-    <StoreListItem v-for="store in stores" :store="store" :key="store.sapStoreID" />
+    <StoreListItem v-for="store in stores" :store="store" />
   </div>
 </template>
