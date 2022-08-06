a35f2f4cb8423d3fd59c2c76bbca2159dcd61259
JCRamires <jcramires851@gmail.com>
2022-08-06 12:37:35 -0300

    responsive map

--- src/components/GoogleMapsIFrame.vue
+++ src/components/GoogleMapsIFrame.vue
@@ -8,14 +8,32 @@ const { latitude, longitude } = defineProps<Props>();
 </script>
 
 <template>
-  <iframe
-    width="800"
-    height="600"
-    frameborder="0"
-    scrolling="no"
-    marginheight="0"
-    marginwidth="0"
-    :src="`https://maps.google.com/maps?q=${latitude},${longitude}&z=14&amp;output=embed`"
-  >
-  </iframe>
+  <div class="iframe-container">
+    <iframe
+      width="800"
+      height="600"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      :src="`https://maps.google.com/maps?q=${latitude},${longitude}&z=14&amp;output=embed`"
+    >
+    </iframe>
+  </div>
 </template>
+
+<style scoped>
+.iframe-container {
+  position: relative;
+  height: 50vh;
+  overflow: hidden;
+}
+
+.iframe-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
