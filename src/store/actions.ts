19558dba74195636b1ffb58d6e2c577f79b8c7d8
Juan Carlos Ramires <jcramires851@gmail.com>
2022-08-07 00:52:06 -0300

    extracting fetch method

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,14 +1,12 @@
 import { ActionTree } from "vuex";
+import { fetchStores } from "../gateways/storesGateway";
 import { State } from "./types";
 
 export const actions: ActionTree<State, any> = {
     async fetchStores(context) {
       context.commit("setIsLoading", true);
-      const response = await fetch(
-        "https://api.jsonstorage.net/v1/json/00000000-0000-0000-0000-000000000000/c4357a15-46e2-4542-8e93-6aa6a0c33c1e"
-      );
-      const storesJson = await response.json();
-      context.commit("setStores", storesJson);
+      const stores = await fetchStores()
+      context.commit("setStores", stores);
       context.commit("setIsLoading", false);
     },
   };
